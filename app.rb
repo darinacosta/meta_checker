63a9a782f76500edd8f2cb7aca8fab0d076ab528
darinacosta <darinjacosta@gmail.com>
2014-05-27 18:32:42 -0500

    Created config.ru

--- app.rb
+++ app.rb
@@ -18,16 +18,18 @@ require_relative 'lib/content_scanner.rb'
 require_relative 'lib/content_set.rb'
 
 #Paths
-get '/' do
-  erb :form
-end
+class MetaChecker < Sinatra::Base
+  get '/' do
+    erb :form
+  end
 
-post '/form' do
-  user_input=params[:URL1]
-  @meta=Scannerset::Scanner.detect(user_input).pull_data(user_input)
-  if @meta.empty?
-  	erb :empty
-  else
-  	erb :output
+  post '/form' do
+    user_input=params[:URL1]
+    @meta=Scannerset::Scanner.detect(user_input).pull_data(user_input)
+    if @meta.empty?
+    	erb :empty
+    else
+    	erb :output
+    end
   end
 end
