69d12465170b47305686846422260c156cdf2a11
darinacosta <darinjacosta@gmail.com>
2014-05-28 14:57:11 -0500

    created an instructions page

--- app.rb
+++ app.rb
@@ -22,6 +22,10 @@ class MetaChecker < Sinatra::Base
     erb :form
   end
 
+  get '/instructions' do
+    erb :instructions
+  end
+
   post '/form' do
     user_input=params[:URL1]
     @meta=Scannerset::Scanner.detect(user_input).pull_data(user_input)
