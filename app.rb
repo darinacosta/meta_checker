5b61fffd6fed28c0760dd43af59d91aafbb3ef8c
darinacosta <darinjacosta@gmail.com>
2014-05-30 23:21:59 -0500

    removed superfluous requirements from app.rb

--- app.rb
+++ app.rb
@@ -1,23 +1,4 @@
-def require_directory(folder)
-  Dir["#{folder}/*"].each {|file| require_relative file }
-end
-
-require 'sinatra'
-require 'mechanize'
-require 'google_drive'
-require 'open-uri'
-
-configure :development do
-  require 'pp'
-  require 'dotenv'
-
-  Dotenv.load
-end
-
-Session = GoogleDrive.login(ENV['GOOGLE_USER'], ENV['GOOGLE_PASS'])
-
-require_directory('lib')
-require_directory('helpers')
+require_relative './requirements.rb'
 
 class MetaChecker < Sinatra::Base
   get '/' do
@@ -27,7 +8,6 @@ class MetaChecker < Sinatra::Base
   get '/instructions' do
     erb :instructions
   end
-
 
   post '/form' do
     user_input=params[:URL1]
