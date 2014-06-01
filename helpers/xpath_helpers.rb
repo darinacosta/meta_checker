0e56e6ec19afdb9ff7f93da58182f132238e3cec
darinacosta <darinjacosta@gmail.com>
2014-06-01 13:40:56 -0500

    fixed typo in xpath helper

--- helpers/xpath_helpers.rb
+++ helpers/xpath_helpers.rb
@@ -1,5 +1,5 @@
 class XpathFunctions
-  def make_xpath_nodeset_case_insensitive(nodeset, str_to_match)
+  def make_xpath_nodeset_case_insensitive(node_set, str_to_match)
     node_set.find_all {|node| node.to_s.downcase == str_to_match.to_s.downcase }
   end
 end
